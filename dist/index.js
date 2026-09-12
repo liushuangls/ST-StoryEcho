565c51251982e763311d596a861cf12371526f86
Liu Shuang <liushuangbill@outlook.com>
2026-09-12 17:37:30 +0800

    Release v0.21.19

--- dist/index.js
+++ dist/index.js
@@ -979,7 +979,7 @@ var MODULE_ID = "story_echo";
 var DISPLAY_NAME = "StoryEcho \xB7 \u5267\u60C5\u4E0A\u4E0B\u6587";
 var CHAT_STATE_VERSION = 3;
 var SETTINGS_VERSION = 12;
-var EXTENSION_VERSION = "0.21.18";
+var EXTENSION_VERSION = "0.21.19";
 
 // src/api/change-events.ts
 var listeners = /* @__PURE__ */ new Set();
